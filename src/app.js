da52a7872538ce771f3697867d81cb0172774efe
davidsobey <davidsobey@live.co.uk>
2017-12-05 20:03:24 +0200

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// import validator from "validator";
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import './styles/styles.scss';
+
+ReactDOM.render(<p>asd</p>, document.getElementById("app"));
