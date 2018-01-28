14796b667c8b910b4b2853330e7c792075cbce8f
davidsobey <davidsobey@live.co.uk>
2018-01-28 10:41:15 +0200

    Login + POST and GET Course

--- src/Containers/Login/index.js
+++ src/Containers/Login/index.js
@@ -11,7 +11,8 @@ import Card from "../../Components/Card";
 import Button from "../../Components/Button";
 import UserActions from "../../Actions/UserActions";
 
-import Logo from "../../Images/LogoText.png";
+import Logo from "../../Images/logoText.png";
+
 const validate = values => {
   const errors = {};
   if (!values.password) {
